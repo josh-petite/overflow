b2301a1df5bbaa9ae7c84ddcea5de95cf05782e8
Josh Petite <josh_petite@symantec.com>
2015-09-14 16:11:37 -0700

    Removed some unused dependencies. Cleaned up bugged jshint flag and fixed issue with auto-open of browser so that the server has completed loading when the open is attempted.

--- gulpfile.js
+++ gulpfile.js
@@ -1,167 +1,133 @@
-var gulp = require('gulp');
-var args = require('yargs').argv;
-var config = require('./gulp.config')();
-var del = require('del');
-var Server = require('karma').Server;
-var csswring = require('csswring');
-var autoprefixer = require('autoprefixer');
-
-var $ = require('gulp-load-plugins')({ lazy: true });
-
-gulp.task('heroku:dev', ['styles', 'build', 'templates', 'libraries']);
-
-gulp.task('test', ['templates'], function (done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done).start();
-});
-
-gulp.task('default', ['styles', 'build', 'templates', 'libraries', 'fonts', 'nodemon', 'app']);
-
-gulp.task('app', function () {
-    gulp.src('')
-        .pipe($.open({ app: 'firefox', uri: 'http://localhost:3000' }));
-});
-
-gulp.task('templates', function () {
-    log('Compiling templates and saving to public...');
-
-    gulp.src(['./src/**/*.html'])
-        .pipe($.html2js({
-            outputModuleName: 'overflow.templates',
-            useStrict: true
-        }))
-        .pipe($.concat('templates.js'))
-        .pipe(gulp.dest('./public/js'))
-});
-
-gulp.task('build', ['clean'], function () {
+(function () {
     'use strict';
 
-    log('Compiling all JS to app.js and saving to public...');
+    var gulp = require('gulp');
+    var args = require('yargs').argv;
+    var config = require('./gulp.config')();
+    var del = require('del');
+    var Server = require('karma').Server;
+    var csswring = require('csswring');
+    var autoprefixer = require('autoprefixer');
 
-    return gulp
-        .src(config.source)
-        .pipe($.concat('app.js'))
-        .pipe($.ngAnnotate())
-        .pipe(gulp.dest('./public/js'));
-});
+    var $ = require('gulp-load-plugins')({ lazy: true });
 
-gulp.task('fonts', function() {
-    'use strict';
+    gulp.task('heroku:dev', ['styles', 'build', 'templates', 'libraries', 'fonts']);
 
-    return gulp
-        .src(config.fonts)
-        .pipe(gulp.dest('./public/fonts'));
-});
-
-gulp.task('nodemon', function () {
-    'use strict';
-
-    $.nodemon({
-        script: './server/server.js',
-        ext: 'js html less',
-        ignore: ['node_modules/**/*.js', 'bower_components/**/*.js', 'public/js/**/*.js'],
-        env: { 'NODE_ENV': 'development' },
-        tasks: ['styles', 'build', 'templates', 'libraries', 'vet']
-    })
-    .on('restart', function () {
-        log('Restarted nodemon!');
+    gulp.task('test', ['templates'], function (done) {
+        new Server({
+            configFile: __dirname + '/karma.conf.js',
+            singleRun: true
+        }, done).start();
     });
-});
 
-gulp.task('clean', ['clean-scripts', 'clean-styles']);
+    gulp.task('default', ['styles', 'build', 'templates', 'libraries', 'fonts', 'nodemon'], function () {
+        gulp.src('')
+            .pipe($.open({ app: 'firefox', uri: 'http://localhost:3000' }));
+    });
 
-gulp.task('clean-scripts', function (done) {
-    'use strict';
+    gulp.task('templates', function () {
+        gulp.src(['./src/**/*.html'])
+            .pipe($.html2js({
+                outputModuleName: 'overflow.templates',
+                useStrict: true
+            }))
+            .pipe($.concat('templates.js'))
+            .pipe(gulp.dest('./public/js'))
+    });
 
-    clean(config.jsTarget, done);
-});
+    gulp.task('build', ['clean'], function () {
+        'use strict';
 
-gulp.task('clean-styles', function (done) {
-    'use strict';
+        return gulp
+            .src(config.source)
+            .pipe($.concat('app.js'))
+            .pipe($.ngAnnotate())
+            .pipe(gulp.dest('./public/js'));
+    });
 
-    clean(config.styleTarget, done);
-});
+    gulp.task('fonts', function () {
+        return gulp
+            .src(config.fonts)
+            .pipe(gulp.dest('./public/fonts'));
+    });
 
-gulp.task('libraries', function () {
-    'use strict';
+    gulp.task('nodemon', function () {
+        $.nodemon({
+            script: './server/server.js',
+            ext: 'js html less',
+            ignore: ['node_modules/**/*.js', 'bower_components/**/*.js', 'public/js/**/*.js'],
+            env: { 'NODE_ENV': 'development' },
+            tasks: ['styles', 'build', 'templates', 'libraries', 'vet']
+        });
+    });
 
-    log('Copying libraries to public...');
+    gulp.task('clean', ['clean-scripts', 'clean-styles']);
 
-    return gulp.src(config.libraries)
-        .pipe(gulp.dest(config.libraryDestination));
-});
+    gulp.task('clean-scripts', function (done) {
+        clean(config.jsTarget, done);
+    });
 
-gulp.task('vet', function () {
-    'use strict';
+    gulp.task('clean-styles', function (done) {
+        clean(config.styleTarget, done);
+    });
 
-    log('Analyzing source with JSHint and JSCS...');
+    gulp.task('libraries', function () {
+        return gulp.src(config.libraries)
+            .pipe(gulp.dest(config.libraryDestination));
+    });
 
-    return gulp
-        .src(config.source)
-        .pipe($.if(args.verbose, $.print()))
-        .pipe($.jscs())
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
-        .pipe($.jshint.reporter('fail'));
-});
+    gulp.task('vet', function () {
+        return gulp
+            .src(config.source)
+            .pipe($.if(args.verbose, $.print()))
+            .pipe($.jscs())
+            .pipe($.jshint())
+            .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
+            .pipe($.jshint.reporter('fail'));
+    });
 
 
-gulp.task('styles', ['style-libraries'], function () {
-    'use strict';
+    gulp.task('styles', ['style-libraries'], function () {
+        var processors = [
+            autoprefixer({ browsers: ['last 2 version', '> 5%'] }),
+            csswring
+        ];
 
-    log('Compiling LESS -> CSS...');
+        return gulp
+            .src(config.less)
+            .pipe($.plumber())
+            .pipe($.less())
+            .pipe($.concat('overflow.css'))
+            .pipe($.postcss(processors))
+            .pipe(gulp.dest(config.styleDestination));
+    });
 
-    var processors = [
-        autoprefixer({ browsers: ['last 2 version', '> 5%'] }),
-        csswring
-    ];
+    gulp.task('style-libraries', function () {
+        return gulp.src(config.styleLibraries)
+            .pipe(gulp.dest(config.styleDestination));
+    });
 
-    return gulp
-        .src(config.less)
-        .pipe($.plumber())
-        .pipe($.less())
-        .pipe($.concat('overflow.css'))
-        .pipe($.postcss(processors))
-        .pipe(gulp.dest(config.styleDestination));
-});
-
-gulp.task('style-libraries', function () {
-    'use strict';
-
-    return gulp.src(config.styleLibraries)
-        .pipe(gulp.dest(config.styleDestination));
-});
-
-gulp.task('clean-styles', function (done) {
-    'use strict';
-
-    var files = config.styleDestination + '**/*.css';
-    clean(files, done);
-});
+    gulp.task('clean-styles', function (done) {
+        var files = config.styleDestination + '**/*.css';
+        clean(files, done);
+    });
 
 ///////////////////////////////////////////////////////////////////////////////
 
-function clean(path, done) {
-    'use strict';
-
-    del(path, done);
-
-    log('Cleaning: ' + $.util.colors.blue(path));
-}
-
-function log(message) {
-    'use strict';
-
-    if (typeof(message) === 'object') {
-        for (var item in message) {
-            if (message.hasOwnProperty(item)) {
-                $.util.log($.util.colors.blue(message[item]));
-            }
-        }
-    } else {
-        $.util.log($.util.colors.blue(message));
+    function clean(path, done) {
+        del(path, done);
+        log('Cleaning: ' + $.util.colors.blue(path));
     }
-}
+
+    function log(message) {
+        if (typeof(message) === 'object') {
+            for (var item in message) {
+                if (message.hasOwnProperty(item)) {
+                    $.util.log($.util.colors.blue(message[item]));
+                }
+            }
+        } else {
+            $.util.log($.util.colors.blue(message));
+        }
+    }
+})();
